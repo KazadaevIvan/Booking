6895d2c8e723837b3b0ab3879ba2692de4a6b178
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-11-23 11:42:31 +0300

    Deleted setUp and tearDown methods

--- src/test/java/tests/BookingSearchTest.java
+++ src/test/java/tests/BookingSearchTest.java
@@ -1,13 +1,7 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import pages.SearchResultsPage;
-
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 @CucumberOptions(
         features = {"classpath:features"},
@@ -17,21 +11,4 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 )
 
 public class BookingSearchTest extends AbstractTestNGCucumberTests {
-    SearchResultsPage searchResultsPage;
-
-    @BeforeMethod
-    public void setUp() {
-        Configuration.browser = "chrome";
-        Configuration.startMaximized = true;
-        Configuration.timeout = 10000;
-        Configuration.headless = true;
-        searchResultsPage = new SearchResultsPage();
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() {
-        if (getWebDriver() != null) {
-            getWebDriver().quit();
-        }
-    }
 }
